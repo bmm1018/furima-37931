c13e0596a891583215ad7756fd9ed68972534a4e
bmm1018 <tototototoopo.mountain@gmail.com>
2022-09-04 21:42:46 +0900

    CSS配置、バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :birthday, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+          validates :nickname, presence: true
+          validates :birthday, presence: true
+          validates :password, format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/, message: "is invalid. Include both letters and numbers"}
+          validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+          validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+          validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
+          validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
+        
 end
