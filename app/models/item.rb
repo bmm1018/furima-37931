c3b8b51574eddfd1dd5ace9e24d4e3b7881e7f17
bmm1018 <tototototoopo.mountain@gmail.com>
2022-09-23 20:40:06 +0900

    エラーメッセージ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,6 +19,7 @@ class Item < ApplicationRecord
     validates :prefectures_id
     validates :delivery_time_id
   end
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half characters" }
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half characters" }
   validates :image, presence: true
 end
