c66ca5899b885e99703a1d62d6436e5d2fad0b04
bmm1018 <tototototoopo.mountain@gmail.com>
2022-10-09 14:57:32 +0900

    テストコードアイテムID取得

--- spec/models/buy_profile_spec.rb
+++ spec/models/buy_profile_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe BuyProfile, type: :model do
   describe '購入情報の保存' do
     before do
       @user = FactoryBot.create(:user)
-      @buy_profile = FactoryBot.build(:buy_profile, user_id: @user.id, item_id: 2)
+      @item = FactoryBot.create(:item)
+      @buy_profile = FactoryBot.build(:buy_profile, user_id: @user.id, item_id: @item.id)
+      sleep(0.5)
     end
 
     context '内容に問題がない場合' do
